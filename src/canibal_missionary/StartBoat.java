6d322b93b21a8bfa967a25d6d1b912b2025f5d0b
gubrian <gubrian@dhcp-v245-045.pv.reshsg.uci.edu>
2014-05-18 22:47:38 -0700

    global variables

--- src/canibal_missionary/StartBoat.java
+++ src/canibal_missionary/StartBoat.java
@@ -2,7 +2,63 @@ package canibal_missionary;
 
 public class StartBoat {
 	
+	private int boatSpace = 3;
+	private int cOnBoat = 0;
+	private int mOnBoat = 0;
+	
+	private int cBoatMax = 0;
+	private int mBoatMax = 0;
+	
+	
+	
+	
+	
+	
+	
+   class cBoat implements Runnable{
+		public int id;
+		
+		public cBoat(int id) {
+			this.id = id;
+		}
+
+		@Override
+		public void run() {
+			
+		}
+		
+	}
+	
+	
+	class mBoat implements Runnable{
+		public int id;
+		
+		public mBoat(int id) {
+			this.id = id;
+		}
+
+		@Override
+		public void run() {
+			
+		}
+		
+	}
+	
+	
+	
+	
 	public static void main(String args[]) {
+		int cannibalNumber = 5;
+		int missionaryNumber = 4;
+		StartBoat can_miss = new StartBoat();
+		
+		for(int i = 0; i < cannibalNumber; i++) {
+			new Thread(can_miss.new cBoat(i)).start();
+		}
+		
+		for(int i = 0; i < missionaryNumber;i++) {
+			
+		}
 		
 	}
 
