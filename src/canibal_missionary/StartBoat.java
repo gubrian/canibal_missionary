137f059e4eba723214a3b0baf45ccaa0315d7367
gubrian <gubrian@dhcp-v245-045.pv.reshsg.uci.edu>
2014-05-18 23:14:12 -0700

    Finish the Structure

--- src/canibal_missionary/StartBoat.java
+++ src/canibal_missionary/StartBoat.java
@@ -1,20 +1,88 @@
 package canibal_missionary;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class StartBoat {
 	
 	private int boatSpace = 3;
 	private int cOnBoat = 0;
 	private int mOnBoat = 0;
 	
+	final Lock lock = new ReentrantLock();
+	final Condition m = lock.newCondition();
+	final Condition c = lock.newCondition();
+	
 	private int cBoatMax = 0;
 	private int mBoatMax = 0;
 	
+	String message = "";
+	
+	public StartBoat(int cOnBoat, int mOnBoat) {
+		this.cOnBoat = cOnBoat;
+		this.mOnBoat = mOnBoat;
+	}
+	
+	/**
+	 * Thread to process cannibal.
+	 * @throws InterruptedException 
+	 */
+	public void cannibalArrive() throws InterruptedException {
+		lock.lock();
+		if(cBoatMax == 0) {
+			c.await();
+		}
+	}
+	
+	/**
+	 * Thread to process missionary
+	 * @throws InterruptedException
+	 */
+	public void missionaryArrive() throws InterruptedException {
+		lock.lock();
+		if(mBoatMax == 0) {
+			m.await();
+		}
+		
+	}
+	
+	
+	/**
+	 * The thread to make boat leave.
+	 */
+	public void onBoat() {
+		boatSpace = 3;			
+		m.signal();
+		c.signal();	
+		message = "Boat leaving with ";
+		cOnBoat = 0;
+		mOnBoat = 0;
+		calculateMax();
+	}
 	
 	
 	
+	/**
+	 * This function is used to get the maximum number for cannibals and missionaries in 
+	 * the next round.
+	 */
+	private void calculateMax() {
+		if(mOnBoat <= 3) {
+			mBoatMax = mOnBoat;
+			cBoatMax = boatSpace - mBoatMax;
+		} else {
+			if(mOnBoat - 3 > 2 * (cOnBoat % 3)) {
+				mBoatMax = 3;
+			} else {
+				mBoatMax = 2;
+			}
+			cBoatMax = 1;
+		}
+	}
 	
-	
-	
+
+	// The cannibal class
    class cBoat implements Runnable{
 		public int id;
 		
@@ -29,7 +97,7 @@ public class StartBoat {
 		
 	}
 	
-	
+	// The missionary class
 	class mBoat implements Runnable{
 		public int id;
 		
@@ -45,21 +113,16 @@ public class StartBoat {
 	}
 	
 	
-	
-	
 	public static void main(String args[]) {
 		int cannibalNumber = 5;
 		int missionaryNumber = 4;
-		StartBoat can_miss = new StartBoat();
-		
+		StartBoat can_miss = new StartBoat(cannibalNumber, missionaryNumber);
 		for(int i = 0; i < cannibalNumber; i++) {
 			new Thread(can_miss.new cBoat(i)).start();
 		}
-		
 		for(int i = 0; i < missionaryNumber;i++) {
-			
+			new Thread(can_miss.new mBoat(i)).start();
 		}
-		
 	}
 
 }
